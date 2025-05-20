375246ce9cfda2dff39495f4a9ddbe62d21e6f17
Bharadwaj Tadikonda <bharadwaj.ananda2000@gmail.com>
2025-05-20 16:20:31 +0530

    Update tracing.py

--- langchain-stock-sentiment/src/services/tracing.py
+++ langchain-stock-sentiment/src/services/tracing.py
@@ -3,7 +3,5 @@ from langfuse.callback import CallbackHandler
 from dotenv import load_dotenv
 
 load_dotenv(override=True)  # Load env variables
-# Make sure you're explicitly pointing to the root-level `.env`
-# load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 langfuse_handler = CallbackHandler()
